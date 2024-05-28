971f3d4688b4995534585ad521fb31d9c573e91d
roman <romasaab@gmail.com>
2024-05-29 02:33:47 +0300

    add contact page && adaptive some sections

--- source/js/components/preloader.js
+++ source/js/components/preloader.js
@@ -1,24 +1,11 @@
-import lottie from 'lottie-web';
 import {addCustomClass} from '../functions/customFunctions';
+const preloader = document.querySelector('.preloader');
 
-const animationData = require('./../preloader.json'),
-    preloader = document.querySelector('.preloader'),
-    
-    options = {
-    container: document.getElementById('loader'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-};
-
-if(preloader && animationData){
-const initPreload = lottie.loadAnimation(options);
-
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function(){
-        addCustomClass(preloader, 'loaded');
-        window.preloaderLoaded = true;
-    },3500)
-});
+if(preloader){
+    document.addEventListener("DOMContentLoaded", function () {
+        setTimeout(function(){
+            addCustomClass(preloader, 'loaded');
+            window.preloaderLoaded = true;
+        },1500)
+    });
 }
